758f1085b3a4dd670073bba6737f396145ff431e
Mackenzie Pech <mackenzieerinpech@gmail.com>
2019-07-19 07:02:59 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@owners = []
+
+  def initialize(species, :dogs => [], :cats => []})
+    @species = species
+    @pets = pets
+    @@owners << self
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def feed_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def self.all
+    @@owners
+  end
+
 end
