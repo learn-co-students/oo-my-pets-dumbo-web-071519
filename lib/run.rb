e592a222fbad0c332961c60f1d77ef8df8c0998a
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-19 12:00:30 -0400

    Done.

---
+++ lib/run.rb
@@ -0,0 +1,11 @@
+# require_relative 'cat.rb'
+# require_relative 'dog.rb'
+# require_relative 'owner.rb'
+# require 'pry'
+#
+#
+# ashna = Owner.new("Ashna")
+# ashna.buy_cat("Crookshanks")
+# ashna.buy_cat("Tom")
+# ashna.sell_pets
+# binding.pry
